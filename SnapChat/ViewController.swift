69c651c9aa4d34239217e56020a0ae1a1c8c6fa7
WillyBaldeon <alex.memito@hotmail.com>
2019-05-09 14:05:25 -0500

    Initial Commit

---
+++ SnapChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnapChat
+//
+//  Created by Tecsup on 9/05/19.
+//  Copyright © 2019 WillyBaldeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
